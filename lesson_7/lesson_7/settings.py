d7795489fda07c9837669de230ded8959ecbba8b
N1kSoSmartGenius <137533909+N1kSoSmartGenius@users.noreply.github.com>
2023-12-11 21:20:16 +0300

    Update settings.py

--- lesson_7/lesson_7/settings.py
+++ lesson_7/lesson_7/settings.py
@@ -121,19 +121,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
 MEDIA_URL = '/media/'
 STATIC_ROOT = '/home/N1kGenius/static'
 MEDIA_ROOT = '/home/N1kGenius/media'
 
 
-STATICFILES_DIRS = [
-    Path.joinpath(BASE_DIR, 'static')
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_ROOT = Path.joinpath(BASE_DIR, 'media/')
